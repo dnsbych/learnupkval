bcc8c5a6b80c75d67ba56ec169d938594e536f99
Denis <dnsbych@gmail.com>
2022-02-16 17:39:52 +0300

    Новые тесты jacoco

--- src/test/java/StorageTest.java
+++ src/test/java/StorageTest.java
@@ -45,4 +45,84 @@ public class StorageTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void checkGet2()
+    {
+        IndexStorage st = new IndexStorage(50);
+
+        int expected = 1;
+        int actual = st.get(1);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkEGet1()
+    {
+        IndexStorage st = new IndexStorage(50);
+
+        Throwable exception = org.junit.jupiter.api.Assertions
+                .assertThrows(IndexOutOfBoundsException.class, () -> st.get(50));
+
+        Assertions.assertEquals("Неправильно указан индекс", exception.getMessage());
+    }
+
+    @Test
+    public void checkEGet2()
+    {
+        IndexStorage st = new IndexStorage(50);
+
+        Throwable exception = org.junit.jupiter.api.Assertions
+                .assertThrows(IndexOutOfBoundsException.class, () -> st.get(-1));
+
+        Assertions.assertEquals("Неправильно указан индекс", exception.getMessage());
+    }
+
+
+    @Test
+    public void checkISSize1()
+    {
+        IndexStorage st = new IndexStorage(50);
+
+        int expected = 50;
+        int actual = st.size();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkISSize2()
+    {
+        IndexStorage st = new IndexStorage(0);
+
+        Throwable exception = org.junit.jupiter.api.Assertions
+                .assertThrows(IllegalArgumentException.class, () -> st.size());
+
+        Assertions.assertEquals("Размер указан неправильно 0", exception.getMessage());
+    }
+
+    @Test
+    public void checkReverse()
+    {
+        IndexStorage st = new IndexStorage(50); // [0,1,2,3,4,5..49]
+
+        // Сравним первый элемент
+        int expected = 49;
+        int actual = st.reverse()[0]; // [49,48,47,46,45..0]
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void checkGetData()
+    {
+        IndexStorage st = new IndexStorage(50); // [0,1,2,3,4,5..49]
+
+        // Сравним первый элемент
+        int expected = 1;
+        int actual = st.getData()[1];
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
